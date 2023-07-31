660eef6fcb34e8ba64c76dbe2fc972b5a5e02090
bitsima <betulse42@gmail.com>
2023-07-31 17:25:55 +0300

    chore: add logging to module

--- utils/git_utils.py
+++ utils/git_utils.py
@@ -2,11 +2,15 @@ import subprocess
 import os
 from typing import Optional
 
+import logging_config
+
+logger = logging_config.setup_logging()
+
 
 def clone_repository(repo_url: str, target_dir: str) -> Optional[str]:
     try:
         subprocess.run(["git", "clone", repo_url, target_dir])
         return os.path.abspath(target_dir)
     except Exception as e:
-        print(f"Failed to clone the repository: {str(e)}")
+        logger.error(f"Failed to clone the repository: {str(e)}")
         return None
