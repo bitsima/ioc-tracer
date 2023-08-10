edf6563a84297a584205266c350e08c04a8b4cb0
bitsima <betulse42@gmail.com>
2023-08-10 10:52:05 +0300

    fix: fix bug on .git folder and overwriting

--- utils/git_utils.py
+++ utils/git_utils.py
@@ -1,4 +1,3 @@
-import subprocess
 import os
 from typing import Optional
 
@@ -8,9 +7,16 @@ logger = logging_config.setup_logging()
 
 
 def clone_repository(repo_url: str, target_dir: str) -> Optional[str]:
+    repo_name = repo_url.rsplit("/", 1)[1].split(".")[0]
+    if os.path.exists(target_dir + "/" + repo_name):
+        os.system(
+            f"git config --global --add safe.directory {target_dir + '/' + repo_name}"
+        )
+        os.system(f"cd {target_dir + '/' + repo_name} && git pull")
+        return repo_name
     try:
-        subprocess.run(["git", "clone", repo_url, target_dir])
-        return os.path.abspath(target_dir)
+        os.system(f"git clone {repo_url} {target_dir + '/' + repo_name}")
+        return repo_name
     except Exception as e:
         logger.error(f"Failed to clone the repository: {str(e)}")
         return None
