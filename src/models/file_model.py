45b699ea039789f909022cfc19d47411f422d71c
bitsima <betulse42@gmail.com>
2023-08-10 10:55:00 +0300

    fix: added new field and fixed import

--- src/models/file_model.py
+++ src/models/file_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, Text
+from sqlalchemy import Column, Integer, Text, JSON
 from sqlalchemy.orm import relationship
 from ..database_setup import Base
 
@@ -8,6 +8,8 @@ class File(Base):
     file_id = Column(Integer, primary_key=True)
     file_name = Column(Text, nullable=False)
     file_path = Column(Text, nullable=False)
+
+    analysis_results = Column(JSON, nullable=True)
 
     file_hashes = relationship(
         "Hash", back_populates="file", cascade="all, delete-orphan"
