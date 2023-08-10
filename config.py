2d136a1e43e552e604364c489e2bf09d0759a46d
bitsima <betulse42@gmail.com>
2023-08-10 10:52:39 +0300

    feat: repo and path env variables

--- config.py
+++ config.py
@@ -15,3 +15,9 @@ DATABASE_URL = (
 virustotal_api_key = os.getenv("VIRUSTOTAL_API_KEY")
 
 db_init_repo = os.getenv("DATABASE_INIT_REPO")
+
+cloned_repo_path = os.getenv("CLONED_REPO_PATH")
+
+processed_data_path = os.getenv("PROCESSED_DATA_PATH")
+
+processed_data_filename = os.getenv("PROCESSED_DATA_FILENAME")
