2a893099559e872efcb88c18f68a2b03e8ce1d11
bitsima <betulse42@gmail.com>
2023-08-11 16:36:07 +0300

    chore: add alienvault configs

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+from OTXv2 import OTXv2
 
 import os
 
@@ -14,6 +15,8 @@ DATABASE_URL = (
 
 virustotal_api_key = os.getenv("VIRUSTOTAL_API_KEY")
 
+alienvault_api_key = os.getenv("ALIENVAULT_API_KEY")
+
 db_init_repo = os.getenv("DATABASE_INIT_REPO")
 
 cloned_repo_path = os.getenv("CLONED_REPO_PATH")
@@ -21,3 +24,6 @@ cloned_repo_path = os.getenv("CLONED_REPO_PATH")
 processed_data_path = os.getenv("PROCESSED_DATA_PATH")
 
 processed_data_filename = os.getenv("PROCESSED_DATA_FILENAME")
+
+
+otx = OTXv2(alienvault_api_key, server="https://otx.alienvault.com/")
